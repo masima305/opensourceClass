58bd9a4077887d1420d5ea81b970dd517d42b703
masima305 <masima305@gmail.com>
2017-03-09 15:30:34 +0900

    name add 2

--- name.c
+++ name.c
@@ -4,11 +4,15 @@ int main (void){
     
     int i;
     
-    printf("please insert 1");
+    printf("please insert 1 or 2");
     scanf("%d",&i);
     
     if(i==1){
         printf("JeongHoon Lee");
+    } else if (i==2){
+        printf("I am a man who rich")
+    } else {
+        printf("fail to print (wrong input)")
     }
     return 0;
 }
