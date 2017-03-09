6bb85147db3df450dd0ffcbf14b058a70f41e85f
masima305 <masima305@gmail.com>
2017-03-09 15:27:41 +0900

    name

---
+++ name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (void){
+    
+    int i;
+    
+    printf("please insert 1");
+    scanf("%d",&i);
+    
+    if(i==1){
+        printf("JeongHoon Lee");
+    }
+    return 0;
+}
